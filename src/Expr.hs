cf94fc2688499b1c1821ccaaaad22fcc9e7a54d8
Andy Tatman <andytathuawei@gmail.com>
2023-10-12 12:26:13 +0200

    Added syntax from paper

--- src/Expr.hs
+++ src/Expr.hs
@@ -1,6 +1,8 @@
 module Expr
-  ( Expr (..)
-  , BinOp (..)
+  ( 
+    -- Expr (..)
+  -- , 
+  BinOp (..)
   , Pred (..)
 
   , Vars (..)
@@ -13,15 +15,15 @@ module Expr
 import Data.Set (Set)
 import qualified Data.Set as Set
 
--- | Expressions are either of type integer or array
-data Expr a
-  = Var a
---   | Array a
-  | Const Integer
-  | BinOp BinOp (Expr a) (Expr a)
---   | Select (Expr a) (Expr a)
---   | Store (Expr a) (Expr a) (Expr a)
-  deriving (Eq, Ord, Show, Functor, Foldable, Traversable)
+-- -- | Expressions are either of type integer or array
+-- data Expr a
+--   = Var a
+--   | Const Integer
+--   | BinOp BinOp (Expr a) (Expr a)
+-- --   | Array a
+-- --   | Select (Expr a) (Expr a)
+-- --   | Store (Expr a) (Expr a) (Expr a)
+--   deriving (Eq, Ord, Show, Functor, Foldable, Traversable)
 
 -- | Binary expression operations.
 data BinOp
@@ -34,47 +36,65 @@ data BinOp
 
 -- | Predicates are of type boolean
 data Pred a
-  = Expr a :==: Expr a
-  -- ^ Equals
-  | Expr a :>=: Expr a
-  -- ^ Less than or equals
-  | Expr a :<=: Expr a
-  -- ^ Greater than or equals
+  -- = Expr a :==: Expr a
+  -- -- ^ Equals
+  -- | Expr a :>=: Expr a
+  -- -- ^ Less than or equals
+  -- | Expr a :<=: Expr a
+  -- -- ^ Greater than or equals
+  = Var a
+  | ConstB Bool
+  | ConstI Integer
+  | BinOp BinOp (Pred a) (Pred a)
+  | Conj (Pred a) (Pred a)
+  | Disj (Pred a) (Pred a)
+  | Neg (Pred a)
+  | IfElse (Pred a) (Pred a) (Pred a)
+  | Func (Pred a)
+  deriving (Eq, Ord, Show, Functor, Foldable, Traversable)
+
+data Constraint a
+  = Pred (Pred a)
+  | Conj_C (Constraint a) (Constraint a)
+  | Impl (Pred a) (Pred a) (Constraint a)  -- For all x of type b: p implies c
   deriving (Eq, Ord, Show, Functor, Foldable, Traversable)
 
 class Vars f where
-  vars :: Ord a => f a -> Set (Expr a)
-
-instance Vars Expr where
-  vars = \case
-    v@(Var _) -> Set.singleton v
-    -- a@(Array _) -> Set.singleton a
-    Const _ -> mempty
-    BinOp _ lhs rhs -> vars lhs <> vars rhs
-    -- Select array index -> vars array <> vars index
-    -- Store array index expr -> mconcat [vars array, vars index, vars expr]
+  vars :: Ord a => f a -> Set (Pred a)
 
 instance Vars Pred where
   vars = \case
-    lhs :==: rhs -> vars lhs <> vars rhs
-    lhs :>=: rhs -> vars lhs <> vars rhs
-    lhs :<=: rhs -> vars lhs <> vars rhs
+    v@(Var _) -> Set.singleton v
+    ConstB _ -> mempty
+    ConstI _ -> mempty
+    BinOp _ lhs rhs -> vars lhs <> vars rhs
+    Conj lhs rhs -> vars lhs <> vars rhs
+    Disj lhs rhs -> vars lhs <> vars rhs
+    Neg x -> vars x
+    IfElse left middle right -> vars left <> vars middle <> vars right
+    Func x -> vars x 
+    
+
+-- instance Vars Pred where
+--   vars = \case
+--     lhs :==: rhs -> vars lhs <> vars rhs
+--     lhs :>=: rhs -> vars lhs <> vars rhs
+--     lhs :<=: rhs -> vars lhs <> vars rhs
 
 -- | Substitues an expression for the passed variable
 class Subable s where
-  subst :: Eq a => a -> Expr a -> s a -> s a
+  subst :: Eq a => a -> Pred a -> s a -> s a
 
-instance Subable Expr where
-  subst lookingFor newReplacement (Var a) = if a == lookingFor then newReplacement else Var a
---   subst lookingFor newReplacement (Array a) = if a == lookingFor then newReplacement else Array a
-  subst _ _ (Const a) = Const a
-  subst x y (BinOp oper left right) = BinOp oper (subst x y left) (subst x y right)
---   subst x y (Select left right) = Select (subst x y left) (subst x y right)
---   subst x y (Store left middle right) = Store (subst x y left) (subst x y middle) (subst x y right)
-  -- subst _ _ _ = undefined
-  
 instance Subable Pred where
-  subst x y (left :==: right)= subst x y left :==: subst x y right
-  subst x y (left :>=: right)= subst x y left :>=: subst x y right
-  subst x y (left :<=: right)= subst x y left :<=: subst x y right
+  subst lookingFor newReplacement (Var a) = if a == lookingFor then newReplacement else Var a
+  subst _ _ (ConstB a) = ConstB a
+  subst _ _ (ConstI a) = ConstI a
+  subst x y (BinOp oper left right) = BinOp oper (subst x y left) (subst x y right)
+  -- TODO
+  subst _ _ _ = undefined 
+  
+-- instance Subable Pred where
+--   subst x y (left :==: right)= subst x y left :==: subst x y right
+--   subst x y (left :>=: right)= subst x y left :>=: subst x y right
+--   subst x y (left :<=: right)= subst x y left :<=: subst x y right
   
