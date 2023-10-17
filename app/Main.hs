411e165f9e6c2fa96624dd06e1391b84d28aa9b2
Andy Tatman <andytathuawei@gmail.com>
2023-10-17 17:07:11 +0200

    Stack run should now (try to) parse test.txt

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,12 @@
 module Main (main) where
 
 import Lib
+import Parse
 
 main :: IO ()
-main = someFunc
+-- main = someFunc
 -- main = print $ incr (-1)
+main = do
+    print "Begin"
+    parserMain "test.txt"
+    print "End"
