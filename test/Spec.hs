e1b75e1b22df4102c60b1b41eebb6279039339b6
Yash <yashisrani35@gmail.com>
2023-10-17 22:28:07 +0200

    start at test

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,20 @@
+import Control.Monad
+import Control.Monad.Trans.Maybe
+import Grade
+import qualified ParseSpec
+import Test.HUnit
+import Test.Hrubric
+import Test.Hspec
+
+rubric :: Rubric
+rubric = do
+  criterion "It compiles" (1 / 10) . passOrFail $
+    it "..." $
+      True @?= True
+  criterion "Parse" (9 / 10) ParseSpec.rubric
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = void . runMaybeT $ do
+  result <- MaybeT $ hrubric rubric
+  pretty result
+  autograde result
