6d0b58dfb147fc81bcc679abb4f66016f8e732eb
Yash <yashisrani35@gmail.com>
2023-10-19 02:33:31 +0200

    Would want to build for it to work

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,20 +1,27 @@
-import Control.Monad
-import Control.Monad.Trans.Maybe
-import Grade
-import qualified ParseSpec
-import Test.HUnit
-import Test.Hrubric
-import Test.Hspec
+-- import Control.Monad
+-- import Control.Monad.Trans.Maybe
+-- import Grade
+-- import qualified ParseSpec
+-- import Test.HUnit
+-- import Test.Hrubric
+-- import Test.Hspec
+-- rubric :: Rubric
+-- rubric = do
+--   criterion "It compiles" (1 / 10) . passOrFail $
+--     it "..." $
+--       True @?= True
+--   criterion "Parse" (9 / 10) ParseSpec.rubric
+-- main :: IO ()
+-- main = void . runMaybeT $ do
+--   result <- MaybeT $ hrubric rubric
+--   pretty result
+--   autograde result
+{-# OPTIONS_GHC -F -pgmF htfpp #-}
 
-rubric :: Rubric
-rubric = do
-  criterion "It compiles" (1 / 10) . passOrFail $
-    it "..." $
-      True @?= True
-  criterion "Parse" (9 / 10) ParseSpec.rubric
+module Main (main) where
+
+-- Import modules defining HTF tests like this:
+import {-@ HTF_TESTS @-} ParseSpec
 
 main :: IO ()
-main = void . runMaybeT $ do
-  result <- MaybeT $ hrubric rubric
-  pretty result
-  autograde result
+main = htfMain htf_importedTests -- all tests in modules imported via {-@ HTF_TESTS @-} are available in htf_importedTests
