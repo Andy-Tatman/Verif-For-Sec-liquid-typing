213e322b460e3d48865c226d343172c08dc65bfb
Yash <yashisrani35@gmail.com>
2023-10-21 11:10:01 +0200

    removed tests out of main

--- app/Main.hs
+++ app/Main.hs
@@ -1,87 +1,59 @@
 module Main (main) where
 
-import Parse
-import TCGen
+-- import Lib
+
 import Config
+import Parse
 import System.Exit
-import System.Directory (listDirectory, doesFileExist, doesDirectoryExist, getCurrentDirectory)
-import System.FilePath ((</>), takeExtension)
+import TCGen
 
 resultExit :: [Bool] -> ExitCode
 resultExit rs | and rs = ExitSuccess
 resultExit _ = ExitFailure 1
 
-processDirectory :: FilePath -> IO [Bool]
-processDirectory directory = do
-    fileNames <- listDirectory directory
-    let filePaths = (directory </>) <$> fileNames
-    results <- mapM (processPath directory) filePaths
-    return (concat results)
-
-processPath :: FilePath -> FilePath -> IO [Bool]
-processPath currentDirectory path = do
-    isFile <- doesFileExist path
-    isDir <- doesDirectoryExist path
-    if isFile && takeExtension path == ".txt"
-        then do
-            result <- trySingleFile path
-            return [result]
-        else if isDir
-            then processDirectory path
-            else do
-                print $ "Error: Could not process file or directory at " ++ path
-                print $ "Current directory: " ++ currentDirectory
-                return []
-
 trySingleFile :: FilePath -> IO Bool
 trySingleFile path = do
-    fileText <- readFile path
-    let parserResult = parserMain fileText
-    case parserResult of
-        Left x -> do
-            print path
-            print "Error: Could not parse provided file."
-            return False
-        Right y -> do
-            print path
-            var <- checker y
-            -- print $ if var then "Valid" else "Invalid"
-            putStrLn $ "The file " <> path <> " is "<> (if var then "valid." else "invalid.") 
-            return var
-
+  fileText <- readFile path
+  let parserResult = parserMain fileText
+  case parserResult of
+    Left x -> do
+      -- ERROR
+      print "Error: Could not parse provided file."
+      print x
+      return False
+    Right y -> do
+      -- Parsed correctly
+      print "Succesfully parsed provided file!"
+      -- print y
+      var <- checker y
+      -- print $ if var then "Valid" else "Invalid"
+      putStrLn $ "The file " <> path <> " is " <> (if var then "valid." else "invalid.")
+      return var
 
 main :: IO ()
+-- main = someFunc
+-- main = print $ incr (-1)
+-- main = do
+--     print "Begin"
+--     fileText <- readFile "test.txt" -- TODO: have this be a call argu
+--     -- let parserResult = parserMain fileText
+--     -- case parserResult of
+--     --     Nothing -> print "Failed to parse provided string."
+--     let parserResult = parserMain fileText
+--     case parserResult of
+--         Left x -> do -- ERROR
+--             print "Error: Could not parse provided file."
+--             print x
+--             -- TODO: do next stuff
+--         Right y -> do -- Parsed correctly
+--             print "Succesfully parsed provided file!"
+--             print y
+--             var <- checker y
+--             print $ if var then "Valid" else "Invalid"
+--             -- print "Checker success"
 main = do
-    cfg <- readConfig
-    -- Pos cases
-    let posdirectory = "programs/pos"  
-    posNames <- listDirectory posdirectory
-    let posfilePaths = (posdirectory </>) <$> posNames
-    rspos <- mapM trySingleFile posfilePaths
-    let trueCount = length $ filter id rspos
-    print $ "Total correct pos: " ++ show trueCount
-    -- Neg cases
-    let negdirectory = "programs/neg"  
-    negNames <- listDirectory negdirectory
-    let negfilePaths = (negdirectory </>) <$> negNames
-    rsneg <- mapM trySingleFile negfilePaths
-    let falseCount = 4 - (length $ filter id rsneg)
-    print $ "Total correct neg: " ++ show falseCount
-    -- Pos cases
-    let posParsedirectory = "programs/posParse"  
-    posParseNames <- listDirectory posParsedirectory
-    let posParsefilePaths = (posParsedirectory </>) <$> posParseNames
-    rsposParse <- mapM trySingleFile posParsefilePaths
-    let trueParseCount = length $ filter id rsposParse
-    print $ "Total correct pos parse: " ++ show trueParseCount
-    -- Neg cases
-    let negParsedirectory = "programs/negParse"  
-    negParseNames <- listDirectory negParsedirectory
-    let negParsefilePaths = (negParsedirectory </>) <$> negParseNames
-    rsnegParse <- mapM trySingleFile negParsefilePaths
-    let falseParseCount = 11 - (length $ filter id rsnegParse)
-    print $ "Total correct neg: " ++ show falseParseCount
+  cfg <- readConfig
+  rs <- mapM trySingleFile $ files cfg
+  exitWith $ resultExit rs
 
-    --To end
-    let rs = rspos ++ rsneg ++ rsposParse ++ rsnegParse
-    exitWith $ resultExit rs
+-- print "End"
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,27 +1,63 @@
--- import Control.Monad
--- import Control.Monad.Trans.Maybe
--- import Grade
--- import qualified ParseSpec
--- import Test.HUnit
--- import Test.Hrubric
--- import Test.Hspec
--- rubric :: Rubric
--- rubric = do
---   criterion "It compiles" (1 / 10) . passOrFail $
---     it "..." $
---       True @?= True
---   criterion "Parse" (9 / 10) ParseSpec.rubric
--- main :: IO ()
--- main = void . runMaybeT $ do
---   result <- MaybeT $ hrubric rubric
---   pretty result
---   autograde result
-{-# OPTIONS_GHC -F -pgmF htfpp #-}
-
 module Main (main) where
 
--- Import modules defining HTF tests like this:
-import {-@ HTF_TESTS @-} ParseSpec
+import Config
+import Parse
+import System.Directory (listDirectory)
+import System.Exit
+import System.FilePath ((</>))
+import TCGen
+
+resultExit :: [Bool] -> ExitCode
+resultExit rs | and rs = ExitSuccess
+resultExit _ = ExitFailure 1
+
+trySingleFile :: FilePath -> IO Bool
+trySingleFile path = do
+  fileText <- readFile path
+  let parserResult = parserMain fileText
+  case parserResult of
+    Left x -> do
+      print path
+      print "Error: Could not parse provided file."
+      return False
+    Right y -> do
+      print path
+      var <- checker y
+      -- print $ if var then "Valid" else "Invalid"
+      putStrLn $ "The file " <> path <> " is " <> (if var then "valid." else "invalid.")
+      return var
 
 main :: IO ()
-main = htfMain htf_importedTests -- all tests in modules imported via {-@ HTF_TESTS @-} are available in htf_importedTests
+main = do
+  -- Pos cases
+  let posdirectory = "programs/pos"
+  posNames <- listDirectory posdirectory
+  let posfilePaths = (posdirectory </>) <$> posNames
+  rspos <- mapM trySingleFile posfilePaths
+  let trueCount = length $ filter id rspos
+  print $ "Total correct pos: " ++ show trueCount
+  -- Neg cases
+  let negdirectory = "programs/neg"
+  negNames <- listDirectory negdirectory
+  let negfilePaths = (negdirectory </>) <$> negNames
+  rsneg <- mapM trySingleFile negfilePaths
+  let falseCount = 4 - (length $ filter id rsneg)
+  print $ "Total correct neg: " ++ show falseCount
+  -- Pos cases
+  let posParsedirectory = "programs/posParse"
+  posParseNames <- listDirectory posParsedirectory
+  let posParsefilePaths = (posParsedirectory </>) <$> posParseNames
+  rsposParse <- mapM trySingleFile posParsefilePaths
+  let trueParseCount = length $ filter id rsposParse
+  print $ "Total correct pos parse: " ++ show trueParseCount
+  -- Neg cases
+  let negParsedirectory = "programs/negParse"
+  negParseNames <- listDirectory negParsedirectory
+  let negParsefilePaths = (negParsedirectory </>) <$> negParseNames
+  rsnegParse <- mapM trySingleFile negParsefilePaths
+  let falseParseCount = 11 - (length $ filter id rsnegParse)
+  print $ "Total correct neg: " ++ show falseParseCount
+
+  -- To end
+  let rs = rspos ++ rsneg ++ rsposParse ++ rsnegParse
+  exitWith $ resultExit rs
