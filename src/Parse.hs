bd8d3bd437d3bbcdafa64a4c27431b878c410a3f
Andy Tatman <andytathuawei@gmail.com>
2023-10-26 13:06:49 +0200

    New tests + bug fix

--- src/Parse.hs
+++ src/Parse.hs
@@ -116,7 +116,7 @@ statementParser = many space >> (
 --  Expr' = "+" subExpr Expr' | "-" subExpr Expr' | e
 -- subExpr = atom subExpr'
 --  subExpr' = "*" atom subExpr' | "/" atom subExpr' | "%" atom subExpr' | e
--- atom = Const Int | Variable | "(" Expr ")" | "-" Expr | "if" pred "then" Expr "else" Expr
+-- atom = Const Int | Variable | "(" Expr ")" | "-" atom | "if" pred "then" Expr "else" Expr
 --  (We have implemented the parser slightly differently to this grammar, to accommodate Haskell.)
 expressionParser :: Parser (Expr String)
 expressionParser = many space >> (
@@ -152,7 +152,7 @@ subExprParser_Accent = many space >> (
 atomExprParser :: Parser (Expr String)
 atomExprParser = many space >> (
     try (char '(' >> many space >> expressionParser <* many space <* char ')') <|> -- (Expr)
-    try (Minus <$> (char '-' >> many space >> expressionParser) ) <|> -- UnaryMinus
+    try (Minus <$> (char '-' >> many space >> atomExprParser) ) <|> -- UnaryMinus
     try (If <$> (string "if" >> many1 space >> predicateParser) 
         <*> (many space >> string "then" >> many1 space >> expressionParser) 
         <*> (many space >> string "else" >> many1 space >> expressionParser) ) <|> -- If
